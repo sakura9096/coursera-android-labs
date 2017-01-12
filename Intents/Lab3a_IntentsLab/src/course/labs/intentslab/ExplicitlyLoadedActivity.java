84a1c61f0e9bd01ce0df0cee235a4de673f2acfd
sakura9096 <yue.zhang.ivy@gmail.com>
2017-01-12 11:51:59 -0500

    Lab3a My Submission

--- Intents/Lab3a_IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
+++ Intents/Lab3a_IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -47,12 +47,13 @@ public class ExplicitlyLoadedActivity extends Activity {
 		Log.i(TAG,"Entered enterClicked()");
 		
 		// TODO - Save user provided input from the EditText field
-
+		String input = mEditText.getText().toString();
 		// TODO - Create a new intent and save the input from the EditText field as an extra
-		
+		Intent intent = new Intent();
+		intent.putExtra("URL", input);
 		// TODO - Set Activity's result with result code RESULT_OK
-		
+		this.setResult(RESULT_OK, intent);
 		// TODO - Finish the Activity
-
+		this.finish();
 	}
 }
